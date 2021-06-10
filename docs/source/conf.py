a15e2ddc58a6d8b58c55242422744327e98a71d9
toddsherwood-EPA <sherwood.todd@epa.gov>
2021-06-10 18:29:01 -0400

    Bump version: 0.1.0 → 0.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ project = 'EPA_CCEMS_PostProcessingTool'
 copyright = '2021, EPA/OAR/OTAQ/ASD'
 author = 'OTAQ/Assessment and Standards Division'
 
-release = '0.1.0'
+release = '0.1.1'
 
 # -- General configuration ---------------------------------------------------
 
